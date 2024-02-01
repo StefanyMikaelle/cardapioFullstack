b955548d9bf44ef4677b96a7ab61325c7c2a0b31
StefanyMikaelle <stefany_mikaelle@hotmail.com>
2024-01-31 23:55:40 -0300

    Initial commit

---
+++ src/main/java/com/stefanyEngenheiraDev/cardapioFullstack/dtos/FoodRequestDto.java
@@ -0,0 +1,4 @@
+package com.stefanyEngenheiraDev.cardapioFullstack.dtos;
+
+public record FoodRequestDto(String title, String image, Integer price) {
+}
---
+++ src/test/java/com/stefanyEngenheiraDev/cardapioFullstack/CardapioFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanyEngenheiraDev.cardapioFullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
