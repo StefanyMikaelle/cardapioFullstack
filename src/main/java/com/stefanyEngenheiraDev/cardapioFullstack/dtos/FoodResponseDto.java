f2c47b88daacebb6f53fc5f49ec454225d33bb9e
StefanyMikaelle <stefany_mikaelle@hotmail.com>
2024-02-03 00:36:18 -0300

    ajuste nas pastas

--- src/main/java/com/stefanyEngenheiraDev/cardapioFullstack/dtos/FoodResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.stefanyEngenheiraDev.cardapioFullstack.dtos;
-
-import com.stefanyEngenheiraDev.cardapioFullstack.entity.Food;
-
-public record FoodResponseDto(Long id, String title, String image, Integer price) {
-    public FoodResponseDto(Food food){
-        this(food.getId(), food.getTitle(),food.getImage(),food.getPrice());
-    }
-}
--- src/main/java/com/stefanyEngenheiraDev/cardapioFullstack/repository/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stefanyEngenheiraDev.cardapioFullstack.repository;
-
-import com.stefanyEngenheiraDev.cardapioFullstack.entity.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food,Long> {
-
-}
